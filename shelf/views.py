dd38cc607ec783e8a51df070c5fb1ec50b227fe9
Ionut <ionutclaudiu50@yahoo.com>
2023-02-18 20:41:44 +0200

    add genre list based on db

--- shelf/views.py
+++ shelf/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from .models import BookModel
 
 def home_view(request):
-    books = BookModel.objects.all()
-    return render(request, 'home.html', {'books': books})
+    filter = request.GET.get('filter') if request.GET.get('filter') != None else ''
+    books = BookModel.objects.filter(genre__icontains=filter)
+    genres = BookModel.objects.values('genre').distinct()
+    return render(request, 'home.html', {'books': books, 'genres': genres})
