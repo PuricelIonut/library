4bd4a0aaf21aa27c2cd4267f1540313a3defcfcc
Ionut <ionutclaudiu50@yahoo.com>
2023-02-22 12:21:35 +0200

    add search by genre/author/title/language

--- shelf/views.py
+++ shelf/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.db.models import Q
+
 from .models import BookModel
 
 
@@ -16,6 +18,11 @@ def filter_books_view(request, genre):
 
 def search_books_view(request):
     search_term = request.GET.get('search')
-    books = BookModel.objects.filter(title__icontains=search_term)
+    books = BookModel.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(author__icontains=search_term) |
+        Q(genre__icontains=search_term) |
+        Q(language__icontains=search_term)
+    )
     genres = BookModel.objects.values('genre').distinct()
     return render(request, 'home.html', {'books':books, 'genres': genres})
