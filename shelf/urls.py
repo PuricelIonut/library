d25fbc89c21c5914054841f52f08cffe0dd78f4a
ionut <ionutclaudiu50@yahoo.com>
2023-03-02 21:57:06 +0200

    add collapse for pages

--- shelf/urls.py
+++ shelf/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('shelf/book/<int:pk>/', views.book_view, name='book'),
     path('shelf/', views.search_books_view, name='search_books'),
-    path('shelf/filter/genre/<str:genre>/', views.filter_books_view, name='books_filter'),
+    path('shelf/filter/<str:filter_type>/<str:filter_option>/', views.filter_books_view, name='books_filter'),
     ]
--- shelf/views.py
+++ shelf/views.py
@@ -12,11 +12,22 @@ def home_view(request):
     return render(request, 'home.html', {'books': books, 'genres': genres, 'languages':languages})
 
 
-def filter_books_view(request, genre):
-    books = BookModel.objects.filter(genre=genre)
+def filter_books_view(request, filter_type, filter_option):
     genres = BookModel.objects.values('genre').distinct()
     languages = BookModel.objects.values('language').distinct()
-    return render(request, 'home.html', {'books':books, 'genres':genres, 'languages':languages})
+    if filter_type == 'genre':
+        filtered_results  = BookModel.objects.filter(genre=filter_option)
+    elif filter_type == 'language':
+        filtered_results  = BookModel.objects.filter(language=filter_option)
+    elif filter_type == 'pages':
+        if '-' in filter_option:
+            temp = filter_option.split('-')
+            x = range(int(temp[0]), int(temp[1]) + 1)
+        elif '+' in filter_option:
+            x = range(300, 9999)
+        filtered_results = BookModel.objects.filter(pages__range=x)
+
+    return render(request, 'home.html', {'books':filtered_results, 'genres':genres, 'languages':languages})
 
 
 def search_books_view(request):
