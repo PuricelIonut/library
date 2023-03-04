a0615644a6e6903062caaccad26a84c635a12773
Ionut <ionutclaudiu50@yahoo.com>
2023-03-04 20:32:49 +0200

    fixed css for books

--- shelf/models.py
+++ shelf/models.py
@@ -9,14 +9,14 @@ class BookModel(models.Model):
     author = models.CharField(max_length=100, blank=False)
     release_year = models.IntegerField()
     quantity = models.IntegerField(default=0)
-    description = models.TextField( max_length=1000 ,blank=True, null=False)
+    description = models.TextField( max_length=5000 ,blank=True, null=False)
     image = models.ImageField(upload_to='shelf/files/book-covers/', default='shelf/files/default/default_cover.jpeg')
 
-    def add_book(self, quantity_to_add: int):
+    def add_stock(self, quantity_to_add: int):
         return self.quantity + quantity_to_add
 
-    def remove_book(self, quantity_to_remove: int):
+    def remove_stock(self, quantity_to_remove: int):
         if quantity_to_remove > self.quantity:
-            ... # TO DO
+            raise ValueError('Not enough stock!')
         else:
             return self.quantity - quantity_to_remove
