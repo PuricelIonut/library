d8134f51bff80a0103b319b674cd9ba5f67148cd
ionut <ionutclaudiu50@yahoo.com>
2023-04-02 21:26:20 +0300

    working on cart

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+import json
 
+from shelf.models import BookModel
 
 def update_item(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('productId:', productId)
+
+    customer = request.user.customer
+    product = BookModel.objects.get(id=productId)
+    
     return JsonResponse('Item was added', safe=False)
